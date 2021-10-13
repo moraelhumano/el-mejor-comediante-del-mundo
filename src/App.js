898a619a787136f9da882704cd33cac3085fc879
moraelhumano <moraelhumano@gmail.com>
2021-10-13 14:56:34 -0500

    se corrige link de calendario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 
 
-import './App.css';
-import NavBar from './Components/NavBar';
-
-import Home from './Pages/Home';
-import Bio from "./Pages/Bio";
-
-
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,24 +8,33 @@ import {
   Link
 } from "react-router-dom";
 
+import {withRouter} from 'react-router';
+
+
+
+
+import './App.css';
+import NavBar from './Components/NavBar';
+import Home from './Pages/Home';
+import Bio from "./Pages/Bio";
+
+
+
+
 function App() {
   return (
     <>
-   
-
-
-      <Router>
+       <Router>
       <Switch>
         <React.Fragment>
-            <div className="bg-grey">
+            <div>
             <NavBar />
               <Route   path="/" component={Home} exact />
-              {/* <Route path="/alumni" component={Alumni}  /> */}
-              <Route path="/bio" component={Bio}  />
+              <Route path="/calendario" component={Bio}  />
             </div>
         </React.Fragment>
       </Switch>
-    </Router>
+    </Router>      
     </>
     
   );
